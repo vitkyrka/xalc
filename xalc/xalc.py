2c5531f0330be46ae72169bec5045b94560bfc01
Rabin Vincent <rabin@rab.in>
2016-05-13 02:17:41 +0200

    Add command line argument

--- xalc/xalc.py
+++ xalc/xalc.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import argparse
+import sys
+
 from IPython.terminal.embed import InteractiveShellEmbed
 from traitlets.config import Config
 
@@ -7,11 +10,24 @@ from transformer import tests
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cmd', nargs='*',
+                        help='run this command and exit')
+    args = parser.parse_args()
+
     c = Config()
     c.InteractiveShell.confirm_exit = False
 
+    if args.cmd:
+        c.PromptManager.out_template = ''
+        c.HistoryAccessor.enabled = False
+
     ipshell = InteractiveShellEmbed(config=c, banner1='')
     ipshell.extension_manager.load_extension('xalc')
+
+    if args.cmd:
+        ipshell.run_cell(' '.join(args.cmd), store_history=False)
+        sys.exit()
 
     print 'xalc examples/tests:'
     for inp, out in tests:
